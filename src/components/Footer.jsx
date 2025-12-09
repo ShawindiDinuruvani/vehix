e077eed07ad059bb503cb0ae535e6b9296d3b37e
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2025-12-09 16:29:22 +0530

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-5">
+    <footer className="bg-dark text-white text-center py-3">
       &copy; 2025 Vehix. All rights reserved.
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
