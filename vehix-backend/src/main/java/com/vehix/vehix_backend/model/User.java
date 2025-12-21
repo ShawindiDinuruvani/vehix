982349a25de5392e57da7cecbfe600a2a7cac20c
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2025-12-21 16:55:39 +0530

    backend

---
+++ vehix-backend/src/main/java/com/vehix/vehix_backend/model/User.java
@@ -0,0 +1,39 @@
+package com.vehix.vehix_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Full_Name;
+    private String email;
+
+    private String password; // this is important
+
+    // Default constructor
+    public User() {}
+
+    // Constructor with parameters
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // ✅ GETTERS & SETTERS
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }   // ← must exist
+    public void setPassword(String password) { this.password = password; }  // ← must exist
+}
+
---
+++ vehix-backend/src/test/java/com/vehix/vehix_backend/VehixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehix.vehix_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
