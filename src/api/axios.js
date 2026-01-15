99b5cdad1e4b8aff5e84b7aa4ffc71f10896a0b3
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2026-01-15 20:30:48 +0530

    error fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,8 @@
+// src/api/axios.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:5000', // your backend URL
-=======
   baseURL: 'http://localhost:3000', //  backend URL
->>>>>>> 15889db408b4c57764bd93b7b7b15636eb1f4733
 });
 
 export default axiosInstance;
