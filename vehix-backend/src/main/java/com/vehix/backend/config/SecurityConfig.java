6f8166dc7d34eb08a4448ba1fbba7d6e73492287
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2026-01-17 12:53:10 +0530

    Update security Config.java

--- vehix-backend/src/main/java/com/vehix/backend/config/SecurityConfig.java
+++ vehix-backend/src/main/java/com/vehix/backend/config/SecurityConfig.java
@@ -4,12 +4,12 @@ package com.vehix.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import java.util.List;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
 
 
 @Configuration
